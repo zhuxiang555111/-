0b7b7fddb7e3974b1ef8849a94eeddadb46d3064
zhuxiang555111 <zhuxiang555111@hotmail.com>
2017-05-06 11:25:38 +0800

    0.0.2

--- single source shortest path/single source shortest path/Graph.h
+++ single source shortest path/single source shortest path/Graph.h
@@ -1,8 +1,12 @@
 #pragma once
 #include<iostream>
 using namespace std;
-
+template<class Type>
 class Graph
 {
-
+	friend void main(void);
+public:
+	void shortestPath(int);
+private:
+	int n, //the vertex0.0.2
 };
