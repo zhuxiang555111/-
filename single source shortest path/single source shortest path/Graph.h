2d47ebb7198c40e0a7a4596057fcbb313c6b05be
zhuxiang <zhuxiang555111@hotmail.com>
2017-05-06 11:18:14 +0800

    0.0.2

--- single source shortest path/single source shortest path/Graph.h
+++ single source shortest path/single source shortest path/Graph.h
@@ -1,8 +1,30 @@
 #pragma once
 #include<iostream>
 using namespace std;
-
+template<class Type>
 class Graph
 {
-
+	friend void main(void);
+public:
+	void shortestPath(int);
+private:
+	int n,  //vertex number of graph G
+		*prev; //the array of previous vertex
+	Type **c, //the adjancency matrix of graph G
+		*dist;//the array of shortest path
 };
+
+
+template<class Type>
+class minHeapNode
+{
+	friend Graph<Type>;
+public:
+	operator int() const { return length; }
+private:
+	int i; //No. of vertex
+	Type length;//current road length
+};
+
+
+template<class Type>
